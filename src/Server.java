5dcc3c96f7282060846488b8288fed13d30e3199
albakag <jobpoisk2009@yandex.ru>
2018-09-06 14:04:14 +0300

    Server_Client_v.0.2

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) throws InterruptedException {
